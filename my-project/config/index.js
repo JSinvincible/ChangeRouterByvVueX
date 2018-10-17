a8fefe2708550a415949e4d619a0ea27af9590b3
zwc <weichao.zuo@1081519838788313.com>
2018-10-17 11:10:51 +0800

    gir

--- my-project/config/index.js
+++ my-project/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '192.168.199.223', // can be overwritten by process.env.HOST192.168.19.223
+    host: '192.168.1.173', // can be overwritten by process.env.HOST192.168.19.223
     port: 9999, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
